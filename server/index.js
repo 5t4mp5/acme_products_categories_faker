f5a7d5b418d68a8637635bfa0415ce4a85ea51f9
Dave Siegel <dave@Admins-MacBook-Pro.local>
2019-03-05 16:08:35 -0500

    fixed port

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const { db, Product, Category, deleteCat, getCategories } = require("./db/index");
 const seed = require("./db/seed");
 const app = require("./app");
+const PORT = process.env.PORT || 3000;
 
 db.authenticate()
   .then(() => db.sync({force: true}))
   .then(console.log("db sync successful"))
   .then(() => seed())
   .then(() => console.log("db seeded"))
-  .then(() => app.listen(3000, () => {
+  .then(() => app.listen(PORT, () => {
     console.log("I'm listening...");
   }))
   .catch(ex => console.error(ex));
